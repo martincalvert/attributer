a3edbe9ef08b0c05b38af08302c16358ba44968a
Martin <martinlcalvert@gmail.com>
2015-01-14 18:25:51 -0800

    Xcode project created

---
+++ attributer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  attributer
+//
+//  Created by Martin Calvert on 1/14/15.
+//  Copyright (c) 2015 Martin Calvert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ attributer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  attributer
+//
+//  Created by Martin Calvert on 1/14/15.
+//  Copyright (c) 2015 Martin Calvert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
