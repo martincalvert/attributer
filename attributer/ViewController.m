4f084990a73a9776683a0b426a8967b7876d5ad9
Martin <martinlcalvert@gmail.com>
2015-01-15 07:46:46 -0800

    Attributer with notifications

--- attributer/ViewController.m
+++ attributer/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *headline;
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 
 @end
 
@@ -16,12 +19,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    [title setAttributes:@{NSStrokeWidthAttributeName: @3} range:NSMakeRange(0, title.length)];
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(preferredFontChange:) name:UIContentSizeCategoryDidChangeNotification object:nil];
+    [self usePreferredFonts];
+}
+
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter]removeObserver:self name:UIContentSizeCategoryDidChangeNotification object:nil];
+}
+
+- (void)preferredFontChange:(NSNotificationCenter*)notification{
+    [self usePreferredFonts];
+}
+
+- (void)usePreferredFonts{
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
+
+- (IBAction)changeBodyTextToButtonBackgroundColor:(UIButton *)sender {
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName value:sender.backgroundColor range:self.body.selectedRange];
+}
+
+- (IBAction)outlineBodySelection:(UIButton *)sender {
+    [self.body.textStorage addAttributes:@{NSStrokeWidthAttributeName : @-3, NSStrokeColorAttributeName : [UIColor blackColor]} range: self.body.selectedRange];
+}
+
+- (IBAction)unoutlineBodySelection:(UIButton *)sender {
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName range:self.body.selectedRange];
 }
 
 @end
